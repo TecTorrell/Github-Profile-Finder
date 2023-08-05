c5cab7439e3c90089297e7f7eb588d6df29ae03f
TorrellCrafts <toroudo15@gmail.com>
2023-08-05 18:10:32 +0100

    added functionality 1/2

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-console.log('hello world');
+const SEARCH__CONTAINER = document.querySelector('.search__box');
+const SEARCH__BTN = document.querySelector('.search');
+const USERNAME = document.querySelector('.username');
+const NAME = document.querySelector('.name');
+const DATE__JOINED = document.querySelector('.date__joined');
+const BIO = document.querySelector('.user__bio');
+const REPO = document.querySelector('.repo__no');
+const FOLLOWERS = document.querySelector('.followers__no');
+const FOLLOWING = document.querySelector('.following__no');
 
-const GITHUB__URL = 'https://api.github.com/users/TorrellCrafts';
-fetch(GITHUB__URL)
-.then(response => response.json())
-.then(data =>{
+function gitHubUserDataFetch(username) {
+    fetch(`https://api.github.com/users/${username}`)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        USERNAME.innerHTML = data.login;
+        NAME.innerHTML = data.name;
+        DATE__JOINED.innerHTML = data.created_at;
+        BIO.innerHTML = data.bio;
+        REPO.innerHTML = data.public_repos;
+        FOLLOWERS.innerHTML = data.followers;
+        FOLLOWING.innerHTML = data.following;
+    });
+};
 
-    const repoContainer = document.querySelector('.repo');
-    repoContainer.innerHTML = data.company;
-    console.log(data);
-});  
+SEARCH__BTN.addEventListener('click', ()=>{
+    let searchField = document.querySelector('input').value;
+
+    if (searchField === '') {
+        console.log('empty');
+        const SEARCH__BOX = document.querySelector('.search__box')
+        SEARCH__BOX.classList.add('error__animation');
+    } else {
+        gitHubUserDataFetch(`${searchField}`);
+    }
+});
